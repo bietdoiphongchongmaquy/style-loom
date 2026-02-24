6b018ffda8bab25cc5426df430771a8193410e05
Long Tran Quang <longtrq26@gmail.com>
2026-02-24 22:28:26 +0700

    style(format): apply Prettier formatting across codebase

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+
 import { type SanityDocument } from "next-sanity";
 
 import { client } from "@/sanity/client";
@@ -15,10 +16,13 @@ export default async function IndexPage() {
 
   return (
     <main className="container mx-auto min-h-screen max-w-3xl p-8">
-      <h1 className="text-4xl font-bold mb-8">Posts</h1>
+      <h1 className="mb-8 text-4xl font-bold">Posts</h1>
       <ul className="flex flex-col gap-y-4">
         {posts.map((post) => (
-          <li className="hover:underline" key={post._id}>
+          <li
+            className="hover:underline"
+            key={post._id}
+          >
             <Link href={`/${post.slug.current}`}>
               <h2 className="text-xl font-semibold">{post.title}</h2>
               <p>{new Date(post.publishedAt).toLocaleDateString()}</p>
