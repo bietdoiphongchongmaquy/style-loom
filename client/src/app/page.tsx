18810a4db1987d6409b6a575d1beef307a925ecc
Long Tran Quang <longtrq26@gmail.com>
2026-02-24 23:08:29 +0700

    feat(design-system): implement initial design system based on Figma specification

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,35 +1,11 @@
-import Link from "next/link";
+import React from "react";
 
-import { type SanityDocument } from "next-sanity";
-
-import { client } from "@/sanity/client";
-
-const POSTS_QUERY = `*[
-  _type == "post"
-  && defined(slug.current)
-]|order(publishedAt desc)[0...12]{_id, title, slug, publishedAt}`;
-
-const options = { next: { revalidate: 30 } };
-
-export default async function IndexPage() {
-  const posts = await client.fetch<SanityDocument[]>(POSTS_QUERY, {}, options);
-
+const page = () => {
   return (
-    <main className="container mx-auto min-h-screen max-w-3xl p-8">
-      <h1 className="mb-8 text-4xl font-bold">Posts</h1>
-      <ul className="flex flex-col gap-y-4">
-        {posts.map((post) => (
-          <li
-            className="hover:underline"
-            key={post._id}
-          >
-            <Link href={`/${post.slug.current}`}>
-              <h2 className="text-xl font-semibold">{post.title}</h2>
-              <p>{new Date(post.publishedAt).toLocaleDateString()}</p>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </main>
+    <div className="container">
+      <h1 className="font-serif font-bold text-brown-60">TEST</h1>
+    </div>
   );
-}
+};
+
+export default page;
